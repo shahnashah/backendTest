c6618f4ff0e1c113495ea6c0589ab777199d25d6
shahna shah <shahanashah9630@gmail.com>
2025-03-05 15:58:51 +0530

    set5

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://shahanashah9630:GU3V3EJHacczCSed@cluster0.tju8z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/synvilla");
+    const conn = await mongoose.connect("mongodb+srv://shahanashah9630:GU3V3EJHacczCSed@cluster0.tju8z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/sun");
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error);
